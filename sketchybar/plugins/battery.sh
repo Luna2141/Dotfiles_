085a8a2d1939eb5a34c7395341e1ed99ebb3fc6f
Luna2141 <60728436+Luna2141@users.noreply.github.com>
2026-06-24 19:28:07 -0400

    Add files via upload

---
+++ sketchybar/plugins/battery.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+BATT_PERCENT=$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)
+sketchybar --set "$NAME" label="${BATT_PERCENT}%"
---
+++ sketchybar/plugins/clock.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Format: Fri 26 Dec 17:15
+sketchybar --set "$NAME" label="$(date '+%a %d %b %H:%M')"
