d1ceebfa049e2667a49cf1f74b7d97843c2e7269
Luna2141 <60728436+Luna2141@users.noreply.github.com>
2026-07-03 17:09:07 -0400

    Delete sketchybar directory

--- sketchybar/plugins/battery.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-BATT_PERCENT=$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)
-sketchybar --set "$NAME" label="${BATT_PERCENT}%"
--- sketchybar/plugins/clock.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Format: Fri 26 Dec 17:15
-sketchybar --set "$NAME" label="$(date '+%a %d %b %H:%M')"
